ff2ad92e0cdeb3b2a51af1e4170cb37a40250c2e
chandru <chandrashekar.vijayarenu@gmail.com>
2016-07-14 21:14:28 -0700

    adding solution

--- maximumvaluecontiguoussubsequence.py
+++ maximumvaluecontiguoussubsequence.py
@@ -5,9 +5,18 @@ class MaxValContiguous():
         self.input = input
 
     def findMaxVal(self):
-        print input
+        self.length = len(self.input)
+        self.output = []
+        self.output.insert(0, self.input[0])
 
+        for index in xrange(1, self.length):
+            max_till_now = max(
+                self.output[index - 1] + self.input[index],
+                self.input[index]
+            )
+            self.output.insert(index, max_till_now)
+        print self.output
 
-input = [1, 2, 4, -1, 4, 2, 0, 6, 3, 5]
+input = [2, -3, 4]
 x = MaxValContiguous(input=input)
 x.findMaxVal()
