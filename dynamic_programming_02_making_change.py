d8bb30099f1ce6a1d0cd7b57ee22427d04b9a1b1
chandru <chandrashekar.vijayarenu@gmail.com>
2016-07-15 23:49:56 -0700

    making change iterative

--- dynamic_programming_02_making_change.py
+++ dynamic_programming_02_making_change.py
@@ -1,6 +1,5 @@
 #in effecient solution.
 def recMC(coinValueList, change, knownResults):
-    print coinValueList, change, knownResults
     minCoins = change
     if change in coinValueList:
         knownResults[change] = 1
@@ -18,3 +17,14 @@ def recMC(coinValueList, change, knownResults):
 
 print recMC([1, 20, 100], 19, [0]*20)
 
+
+def dpMakeChange(coinValueList,change,minCoins):
+   for cents in range(change+1):
+      coinCount = cents
+      for j in [c for c in coinValueList if c <= cents]:
+            if minCoins[cents-j] + 1 < coinCount:
+               coinCount = minCoins[cents-j]+1
+      minCoins[cents] = coinCount
+   return minCoins[change]
+
+print dpMakeChange([1, 5, 10], 11, [0]*12)
